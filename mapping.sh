423fa922e6151dcee46f802f65862920b9db5c08
bubujenn <blanka.jendriskova0000@seznam.cz>
2025-03-10 18:42:06 +0100

    Update mapping.sh

--- mapping.sh
+++ mapping.sh
@@ -2,8 +2,7 @@
 #PBS -l select=1:ncpus=8:mem=32gb:scratch_local=500gb
 #PBS -l walltime=12:00:00
 #PBS -N Minimap2_Samtools
-#PBS -o /storage/plzen1/home/jendrb00/outputs/minimap2_alignment.stdout
-#PBS -e /storage/plzen1/home/jendrb00/outputs/minimap2_alignment.stderr
+#PBS -j oe
 
 trap 'clean_scratch' TERM EXIT
 
@@ -12,17 +11,17 @@ RESULTDIR="/storage/plzen1/home/jendrb00/results"
 SCRATCHDIR=${SCRATCHDIR:-"/scratch"}
 
 cp $DATADIR/reference.fasta $SCRATCHDIR || exit 1
-cp $DATADIR/query.fasta $SCRATCHDIR || exit 1
-cd $SCRATCHDIR || exit 2
+cp $DATADIR/query.fasta $SCRATCHDIR || exit 2
+cd $SCRATCHDIR || exit 3
 
 module load minimap2
 module load samtools
 
-minimap2 -ax asm5 --secondary=no reference.fasta query.fasta > alignment.sam
+minimap2 -ax asm5 --secondary=no reference.fasta query.fasta > alignment.sam || exit 4
 
-samtools view -F 2048 -h alignment.sam > filtered.sam
-samtools sort -o filtered.bam filtered.sam
-samtools index filtered.bam
-samtools view filtered.bam | awk '{print $3, $1}' | sort | uniq > scaffold_to_chr.tsv
+samtools view -F 2048 -h alignment.sam > filtered.sam || exit 5
+samtools sort -o filtered.bam filtered.sam || exit 6
+samtools index filtered.bam || exit 7
+samtools view filtered.bam | awk '{print $3, $1}' | sort | uniq > scaffold_to_chr.tsv || exit 8
 
-cp -r $SCRATCHDIR/* $RESULTDIR || export CLEAN_SCRATCH=false
+cp -r $SCRATCHDIR/* $RESULTDIR || export CLEAN_SCRATCH=false || exit 9
