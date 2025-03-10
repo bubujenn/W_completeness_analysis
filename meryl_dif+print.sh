0c23c51027e08dfcc8295af12b9afcc2b24d3a07
bubujenn <blanka.jendriskova0000@seznam.cz>
2025-03-10 18:44:05 +0100

    Update meryl_dif+print.sh

--- meryl_dif+print.sh
+++ meryl_dif+print.sh
@@ -24,18 +24,18 @@ export PATH=/storage/plzen1/home/jendrb00/meryl-1.4.1/bin:$PATH
 mkdir -p meryl_results
 
 echo "Porovnání PacBio dat s AA_Zchr..."
-meryl difference $PACBIO_MERYL $AA_Z_MERYL output meryl_results/expected_W_size.meryl || exit 2
+meryl difference $PACBIO_MERYL $AA_Z_MERYL output meryl_results/expected_W_size.meryl || exit 5
 
 echo "Porovnání PacBio dat s AA_Z_W..."
-meryl difference $PACBIO_MERYL $AA_Z_W_MERYL output meryl_results/W_missing_in_assembly.meryl || exit 3
+meryl difference $PACBIO_MERYL $AA_Z_W_MERYL output meryl_results/W_missing_in_assembly.meryl || exit 6
 
 echo "Počítám sumy unikátních k-merů pro očekávanou velikost W chromozomu..."
-meryl print meryl_results/expected_W_size.meryl | awk '{sum += $2} END {print sum}' > meryl_results/expected_W_size_sum.txt || exit 4
+meryl print meryl_results/expected_W_size.meryl | awk '{sum += $2} END {print sum}' > meryl_results/expected_W_size_sum.txt || exit 7
 
 echo "Počítám sumy chybějících unikátních k-merů W v sestaveném genomu..."
-meryl print meryl_results/W_missing_in_assembly.meryl | awk '{sum += $2} END {print sum}' > meryl_results/W_missing_in_assembly_sum.txt || exit 5
+meryl print meryl_results/W_missing_in_assembly.meryl | awk '{sum += $2} END {print sum}' > meryl_results/W_missing_in_assembly_sum.txt || exit 8
 
 mkdir -p $RESULTDIR
-cp -r meryl_results/* $RESULTDIR || export CLEAN_SCRATCH=false
+cp -r meryl_results/* $RESULTDIR || export CLEAN_SCRATCH=false || exit 9
 
-echo "První část dokončena. Výsledky jsou v: $RESULTDIR"
+echo "První část dokončena. Výsledky jsou v: $RESULTDIR" || exit 10
