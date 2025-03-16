d0c9cf1a448ea899bcb0fc6afb4d72601f65cfc7
Blanka Jendriskova <blanka.jendriskova0000@seznam.cz>
2025-03-16 09:04:01 +0100

    Update Merqury_errors_check.sh

--- Merqury_errors_check.sh
+++ Merqury_errors_check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -q interactive@pbs-m1.metacentrum.cz 
-#PBS -l walltime=1:0:0
+#PBS -l walltime=3:0:0
 #PBS -l select=1:ncpus=16:mem=128gb:gpu_mem=128gb:scratch_local=200gb:vnode=kirke59
 #PBS -N merqury_check_eroors_job
 
