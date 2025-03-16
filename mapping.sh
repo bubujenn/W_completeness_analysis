c7488e14dc557e47140f2921469a9e8371a378fe
Blanka Jendriskova <blanka.jendriskova0000@seznam.cz>
2025-03-16 09:47:40 +0100

    Update mapping.sh

--- mapping.sh
+++ mapping.sh
@@ -33,7 +33,6 @@ module load python/3.9.12-gcc-10.2.1
 module load samtools/1.13-gcc-10.2.1
 module load minimap2/2.22-gcc-10.2.1
 
-
 # Run Minimap2 alignment
 minimap2 -ax asm5 --secondary=no "$REFERENCE" "$QUERY" > alignment.sam || exit 4
 
